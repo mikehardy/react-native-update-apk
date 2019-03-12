6614fc1861a08a1c01be6a68fe73ad5310b1e4da
Mike Hardy <github@mikehardy.net>
2019-03-11 23:02:09 -0500

    Integrated example and refresh-example script

--- example/App.js
+++ example/App.js
@@ -8,23 +8,68 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import updateApk from 'rn-update-apk';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const updater = new updateApk({
+  iosAppId: '1104809018', // iOS must use App Store. This is a sample: "All Birds of Ecuador" (¡Qué lindo!)
+  apkVersionUrl: 'https://raw.githubusercontent.com/mikehardy/react-native-update-apk/master/example/test-version.json',
+  needUpdateApp: (needUpdate) => {
+    Alert.alert(
+      'Update Available',
+      'New version released, do you want to update?',
+      [
+        {text: 'Cancel', onPress: () => {}},
+        {text: 'Update', onPress: () => needUpdate(true)}
+      ]
+    );
+  },
+  forceUpdateApp: () => {
+    console.log("forceUpdateApp callback called")
+  },
+  notNeedUpdateApp: () => {
+    console.log("notNeedUpdateApp callback called")
+  },
+  downloadApkStart: () => { console.log("downloadApkStart callback called") },
+  downloadApkProgress: (progress) => { console.log(`downloadApkProgress callback called - ${progress}%...`) },
+  downloadApkEnd: () => { console.log("downloadApkEnd callback called") },
+  onError: () => { console.log("onError callback called") }
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVersion: 'unknown',
+      serverVersion: 'unknown',
+    }
+  }
+
+  async componentDidMount() {
+
+  }
+
+  _onCheckServerVersion = () => {
+    console.log('Check server version');
+  }
+
+  _onCheckServerVersion = () => {
+    updater.checkUpdate();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>rn-update-apk example</Text>
+        <Text style={styles.instructions}>Current APK version: {this.state.currentVersion}</Text>
+        <Button title="Check Server Version" onPress={this._onCheckServerVersion}>
+          <Text style={styles.instructions}>Check Server Version</Text>
+        </Button>
+        <Button title="Update App" onPress={this._onUpdateApp}>
+          <Text style={styles.instructions}>Update App</Text>
+        </Button>
       </View>
     );
   }
---
+++ refresh-example.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e 
+
+echo "You should run this from directory where you have cloned the react-native-update-apk repo"
+echo "You should only do this when your git working set is completely clean (e.g., git reset --hard)"
+echo "You must have already run \`npm install\` in the repository so \`npx react-native\` will work"
+echo "This scaffolding refresh has been tested on macOS, if you use it on linux, it might not work"
+
+# Copy the important files out temporarily
+if [ -d TEMP ]; then
+  echo "TEMP directory already exists - we use that to store files while refreshing."
+  exit 1
+else
+  echo "Saving files to TEMP while refreshing scaffolding..."
+  mkdir -p TEMP/android/app/src/main/
+  cp example/android/local.properties TEMP/android/ || true
+  cp example/android/app/src/main/AndroidManifest.xml TEMP/android/app/src/main/ || true
+  cp example/App.js TEMP/
+  cp example/test-version.json TEMP/
+fi
+
+# Purge the old sample
+\rm -fr example
+
+# Make the new example
+npx react-native init example
+pushd example
+npm install rn-update-apk
+npx react-native link rn-update-apk
+npx react-native link react-native-fs
+
+# Copy the important files back in
+popd
+echo "Copying update-apk example files into refreshed example..."
+cp -frv TEMP/* example/
+
+# Clean up after ourselves
+\rm -fr TEMP
