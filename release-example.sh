37729f905ef49a917eb60709951d408b9afa209e
Mike Hardy <github@mikehardy.net>
2019-03-13 23:08:16 -0500

    Prepare / release example for 3.0.1 as test target

--- release-example.sh
+++ release-example.sh
@@ -2,3 +2,6 @@
 pushd example
 mkdir -p android/app/src/main/assets/
 npx react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+pushd android
+./gradlew assembleDebug
+cp app/build/outputs/apk/debug/app-debug.apk ../rn-update-apk-example-3.0.1.apk
