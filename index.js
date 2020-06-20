6e446bf8718227ebb8be545c0bb11477887d096f
Shubham <shubham@league11.in>
2020-06-20 12:41:07 +0530

    Added what news text

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ export class UpdateAPK {
           if (isUpdate) {
             this.downloadApk(remote);
           }
-        });
+        }, remote.whatsNew);
       }
     } else if (this.options.notNeedUpdateApp) {
       this.options.notNeedUpdateApp();
